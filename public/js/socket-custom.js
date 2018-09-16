824f09d91222ec11d43107b817cd9239bb720f70
MauricioRmrz <ramirezmauricio@hotmail.com>
2018-09-16 14:35:44 -0500

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+// Escuchar
+socket.on('connect', function() {
+
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Mauricio',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+});
